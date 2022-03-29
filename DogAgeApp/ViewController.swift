0600758de07ab397fcfc28f7529b5a05bb2069c4
Gustavo Carvalho <gstcarv@gmail.com>
2022-03-29 19:51:37 -0300

    calculating age and printing on screen

--- DogAgeApp/ViewController.swift
+++ DogAgeApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnCalculateAge: UIButton!
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var fieldAge: UITextField!
+    
+    @IBAction func calculateAge(_ sender: UIButton) {
+        
+        if (fieldAge.text != "") {
+            let humanAge = Int(fieldAge.text!)! * 7;
+            
+            lblResult.text = "A idade do cachorro em anos humanos é: \(humanAge)";
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        print("hey my brothaaa")
     }
 
     
